6eacc954d01213d3f0270dbea3c36187f0980cef
Michael Reifer <Mjreifer1@gmail.com>
2024-12-05 16:32:21 -0500

    allowing large file uplaod

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Use the specified CORS options
 
 // parses requests body
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.use(express.urlencoded({extended: false}));
 
